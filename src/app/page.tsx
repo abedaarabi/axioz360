7ab9fcab43d78cde4b4b9ba601fde282aeee61c8
Abed Aarabi <abedaarabi@Abeds-MacBook-Pro.local>
2024-10-27 16:50:48 +0100

    add cookies banner

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { LandingPageComponent } from "@/components/landing-page";
-
-export default function Home() {
-  return (
-    <div className="">
-      <LandingPageComponent />
-    </div>
-  );
-}
